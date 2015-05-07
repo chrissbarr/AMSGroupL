657b955e3b9deffd28d7fe9ebe35d8071b87c8eb
Chris Barr <spartanbarr@gmail.com>
2015-05-07 13:04:41 +0930

    Odometry Works!
    Wheel odometry is matching Vicon data quite closely. Still need wheel
    calibration.

--- PythonScripts/EncoderToOdometry.py
+++ PythonScripts/EncoderToOdometry.py
@@ -18,8 +18,8 @@ from std_msgs.msg import *
 
 # define settings
 WheelDiameter = 45.0	# wheel diameter in mm
-WheelBase = 225.0		# distance between wheels in mm
-DistancePerCount = (3.14159 *  WheelDiameter) / (3200) # mm / encoder tick
+WheelBase = 230.0 * 2		# distance between wheels in mm
+DistancePerCount = (3.14159 *  WheelDiameter) / (320) # mm / encoder tick
 
 x = 0.0	# x coordinate in mm
 y = 0.0	# y coordinate in mm
@@ -46,16 +46,28 @@ last_time_odom = 0
 
 
 def wheel_callback(data):
+	global delta_left, delta_right, _PreviousLeftEncoderCounts, _PreviousRightEncoderCounts, last_time, th, x, y
+
 	current_time = time.time()
+
+	if(last_time == 0):
+		last_time = time.time()
+		vx = 0
+		vy = 0
+		vth = 0
 
 	left_encoder = data.data[2]
 	right_encoder = data.data[3]
 
-	global delta_left, delta_right, _PreviousLeftEncoderCounts, _PreviousRightEncoderCounts, last_time
+	
 	
 	#calculate the change in encoder values since last message received
-	delta_left = left_encoder - _PreviousLeftEncoderCounts
-	delta_right = right_encoder - _PreviousRightEncoderCounts
+	if( _PreviousLeftEncoderCounts == 0 and _PreviousRightEncoderCounts == 0):
+		delta_left = 0
+		delta_right = 0
+	else:
+		delta_left = left_encoder - _PreviousLeftEncoderCounts
+		delta_right = right_encoder - _PreviousRightEncoderCounts
 	
 	#convert change to distance travelled by each wheel
 	vx = delta_left * DistancePerCount
@@ -66,37 +78,30 @@ def wheel_callback(data):
 
 	print('Wheel callback! LE: %d RE: %d DL: %d DR: %d vx: %.3f vy: %.3f vth: %.3f') % (left_encoder, right_encoder, delta_left, delta_right, vx, vy, vth)
 	
+	# compute odometry
+	dt = (current_time - last_time)	# time difference in seconds
+
+	delta_th = vth * dt
+
+	th = math.atan2(math.sin(th + delta_th ),math.cos(th + delta_th)) 
+
+	delta_x = (vx * math.cos(th)) * dt
+	delta_y = (vy * math.sin(th)) * dt
+	
+	x += delta_x
+	y += delta_y
 
 	#publish calculated values to odometry topic
-	publish_odometry(vx, vy, vth)
+	publish_odometry(x/1000, y/1000, th)
 
 	#save current values for next time
 	_PreviousLeftEncoderCounts = left_encoder
 	_PreviousRightEncoderCounts = right_encoder
 	last_time = current_time
-
-	
 	
 	return
 
-def publish_odometry(vx, vy, vth):
-	global x, y, th, last_time_odom
-
-	if(last_time_odom == 0):
-		last_time_odom = time.time()
-
-	current_time_odom = time.time()
-	
-	# compute odometry
-	dt = (current_time_odom - last_time_odom)# / 1000	# time difference in seconds
-	delta_x = (vx * math.cos(th) - vy * math.sin(th)) * dt
-	delta_y = (vx * math.sin(th) + vy * math.cos(th)) * dt
-	delta_th = vth * dt
-	
-	
-	x += delta_x
-	y += delta_y
-	th += delta_th
+def publish_odometry(x, y, th):
 
 	print('X: %.3f Y: %.3f Theta: %.3f') % (x, y, th)
 	
@@ -107,7 +112,7 @@ def publish_odometry(vx, vy, vth):
 	# create quaternion
 	q = tf.transformations.quaternion_from_euler(0, 0, th)
 	
-	msg.pose.pose.position = Point(x/1000, y/1000, 0)
+	msg.pose.pose.position = Point(x, y, 0)
 	msg.pose.pose.orientation.x = q[0]
 	msg.pose.pose.orientation.y = q[1]
 	msg.pose.pose.orientation.z = q[2]
@@ -118,8 +123,6 @@ def publish_odometry(vx, vy, vth):
 	#msg.twist.twist.angular.z = vth
 
 	odom_pub.publish(msg)
-
-	last_time_odom = current_time_odom
 
 
 def main():

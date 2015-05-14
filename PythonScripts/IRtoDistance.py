4db60cfa2b38b41abb23741e9c3beacb34b7feb9
Chris Barr <spartanbarr@gmail.com>
2015-05-14 10:55:56 +0930

    Updated IRtoDistance
    Updated script to use different equations for each sensor, based on
    sensors measured output. Time checking also added.

--- PythonScripts/IRtoDistance.py
+++ PythonScripts/IRtoDistance.py
@@ -18,6 +18,11 @@ sensor2 = 0
 sensor3 = 0
 sensor4 = 0
 
+# sensors map range to analog value roughly according to power equation (i.e. dist = c * analog^b)
+# below arrays hold 'c' and 'b' for each sensor
+
+sensor_equation_multiplier = [118514, 39041, 81533, 112346]
+sensor_equation_exponent = [-1.252, -1.052, -1.199, -1.251]
 
 rospy.init_node("get_ir", anonymous=False) # name the script on the ROS network
 
@@ -28,18 +33,23 @@ def subscriber():
     
 def ir_update(data):
   
-    sensor1 = ir_to_mm(data.data[0])
-    sensor2 = ir_to_mm(data.data[1])
-    sensor3 = ir_to_mm(data.data[2])
-    sensor4 = ir_to_mm(data.data[3])
+	start_time = time.time()
+	
+    sensor1 = ir_to_mm(data.data[0],1)
+    sensor2 = ir_to_mm(data.data[1],2)
+    sensor3 = ir_to_mm(data.data[2],3)
+    sensor4 = ir_to_mm(data.data[3],4)
     
-    print("Sensor1: %.3f Sensor2: %.3f Sensor3: %.3f Sensor4: %.3f") % (sensor1, sensor2, sensor3, sensor4)
+    time_taken = time.time() - start_time
+    
+    print("Sensor1: %.3f Sensor2: %.3f Sensor3: %.3f Sensor4: %.3f | Time Taken: ") % (sensor1, sensor2, sensor3, sensor4, time_taken)
 
-def ir_to_mm(analog_value):
-    distance_in_mm = 106777 * analog_value ^ (-1.242)
-
+def ir_to_mm(analog_value, sensor_num):
+	# arrays are 0 indexed, but sensor_num is setup for readability (1-4 for sensors)
+	array_index = sensor_num-1
+	#calculate distance according to each sensors profile
+    distance_in_mm = sensor_equation_multiplier[array_index] * pow(analog_value,sensor_equation_exponent[array_index])
     return distance_in_mm
-  
     
 def main():
     ros_update_1 = subscriber() # subscribe to ROS topics
